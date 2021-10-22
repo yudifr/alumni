7039870d604f9f5937152e4e1a343114d4c01505
Yudi Fathurrahman <yfathurrahman@gmail.com>
2021-10-22 10:21:45 +0700

    guest middleware

--- app/Http/Controllers/KuisionerController.php
+++ app/Http/Controllers/KuisionerController.php
@@ -13,19 +13,39 @@ class KuisionerController extends Controller
 }
 protected function validator(array $data)
     {
-        return Validator::make($data, [
-            'name' => 'required|max:255',
-            'email' => 'required|email:filter|max:255|unique:users',
-            'password' => 'required|min:6|confirmed',
-            'jenis' => 'required|max:255',
-            'alamat' => 'required|max:255',
-            'no_telp' => 'required|max:255',
-            'provinsi' => 'required|max:255',
-            'kota' => 'required|max:255',
-            'kecamatan' => 'required|max:255',
-            'kelurahan' => 'required|max:255',
 
-        ]);
+       
     }
+    public function kuisioner(Request $request)
+    {
+        
+       $validator =  Validator::make($request->all(), [
+            'pindah' => 'required|max:2',
+            'alasan_pindah' => 'required|max:255',
+            'bidangkerja' => 'required|max:255',
+            'gaji_sekarang' => 'required|max:255',
+            'gajipertama' => 'required|max:255',
+            'kerja_pertama' => 'required|max:255',
+            'sesuaikah' => 'required|max:255',
+            'syaratipk' => 'required|max:255',
+            'kuisioner1' => 'required|max:255',
+            'kuisioner2' => 'required|max:255',
+            'kuisioner3' => 'required|max:255',
+            'kuisioner4' => 'required|max:255',
+            'kuisioner5' => 'required|max:255',
+            'kuisioner6' => 'required|max:255',
+            'kuisioner7' => 'required|max:255',
+            'kuisioner8' => 'required|max:255',
+            
+        ]);
+        if ($validator->fails()) {
+            return redirect('')
+                        ->withErrors($validator)
+                        ->withInput();
+        }
+
+    }
+    
 }
+
 
